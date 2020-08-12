ac3a79ba848a03791cc87812e394b03bfd6c8ee9
Noah Gaston <nrgman@tds.net>
2020-08-12 15:28:21 -0500

    little more

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,11 @@
+import java.util.Random;
+import java.util.Scanner;
 public class App {
 
     public static void main(String[] args)
     {
         System.out.println("project 0" + (int)0.1);
-        
+        Scanner in = new Scanner(System.in);
     }
     
 }
