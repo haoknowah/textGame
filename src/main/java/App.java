a55b8a8c9fdbafee73972217f8a1e90b89d3fa88
Noah Gaston <nrgman@tds.net>
2020-08-14 14:11:12 -0500

    config

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,9 +7,39 @@ public class App {
     private boolean play;
     public static void main(String[] args)
     {
-        System.out.println("project 0" + (int)0.1);
         Scanner in = new Scanner(System.in);
-        player = new Character();
+        Properties config = setupGameConfig(in);
+        play(in, config);
     }
-    
+    static public Properties setupGameConfig(Scanner in){
+        Properties gameConfig = new Properties();
+        int menuSelect = -1;
+        System.out.print("Enter character name: ");
+        String name = in.nextLine();
+        while(menuSelect != 0){
+            System.out.println("1: Change dungeon (Current == begin)");
+            System.out.println("2: Spell selection");
+            System.out.println("0: Begin game");
+            try {
+                menuSelect = Integer.parseInt(in.nextLine());
+            } catch (Exception InputMismatchException) {
+                System.out.println("Enter a number you dolt.");
+            }
+            if(menuSelect <0 || menuSelect > 2){
+                System.out.println("Invalid choice.");
+            }
+            System.out.println(menuSelect);
+            switch(menuSelect){
+                case 1:
+                break;
+                case 2:
+                break;
+            }
+        }
+        //save character info to database or file
+        return gameConfig;
+    }
+    static public void play(Scanner in, Properties config){
+        
+    }
 }
